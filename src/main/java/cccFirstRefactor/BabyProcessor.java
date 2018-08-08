4aab821600a038a852bd98f30263c9399202e460
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-08-07 21:41:17 -0400

    message for baby processor

--- src/main/java/cccFirstRefactor/BabyProcessor.java
+++ src/main/java/cccFirstRefactor/BabyProcessor.java
@@ -12,6 +12,7 @@ public class BabyProcessor implements Processor {
 
     @Override
     public String executeAndReturnResult(Data data) {
+        System.out.println("Processing data using BabyProcessor...");
         return "Baby";
     }
 }
