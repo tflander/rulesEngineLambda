1b0184a8a7f98e27493f57da28e32dc83bdbcdd3
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-08-07 23:04:40 -0400

    break out processor functions

---
+++ src/main/java/eeethirdRefactor/ProcessorFunctions.java
@@ -0,0 +1,20 @@
+package eeethirdRefactor;
+
+public class ProcessorFunctions {
+
+    public static String processDataForBaby(Data data) {
+        System.out.println("Processing data for baby...");
+        return "Baby";
+    }
+
+    public static String processDataForToddler(Data data) {
+        System.out.println("Processing data for toddler...");
+        return "Toddler";
+    }
+
+    public static String processDataForHuman(Data data) {
+        System.out.println("Processing data for human...");
+        return "Human";
+    }
+
+}
