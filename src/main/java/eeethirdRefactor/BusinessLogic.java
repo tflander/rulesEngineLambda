fa5ec4d306854877a239b547ae8d00b0728ef5db
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-08-08 18:00:59 -0400

    properly handle generics in builder

--- src/main/java/eeethirdRefactor/BusinessLogic.java
+++ src/main/java/eeethirdRefactor/BusinessLogic.java
@@ -1,27 +1,25 @@
 package eeethirdRefactor;
 
 import eeethirdRefactor.executionFlow.FirstMatchingRuleDataFlow;
-import eeethirdRefactor.executionFlow.FirstMatchingRuleDataFlowBuilder;
 import model.Data;
 
 import java.util.function.Predicate;
 
-@SuppressWarnings("unchecked")
 public class BusinessLogic {
 
-    private final ProcessorFunctions functions = new ProcessorFunctions();
+    private ProcessorFunctions functions = new ProcessorFunctions();
+
     private final Predicate<Data> isBaby = isAgeInRange(0, 2);
     private final Predicate<Data> isToddler = isAgeInRange(3, 5);
     private final Predicate<Data> isHuman = isAgeInRange(6, Integer.MAX_VALUE);
 
     public FirstMatchingRuleDataFlow<Data, String> getFlow() {
 
-        return FirstMatchingRuleDataFlowBuilder.create()
-                .addingCondition(isBaby).toRun(data -> functions.processDataForBaby((Data)data))
-                .addingCondition(isToddler).toRun(data -> functions.processDataForToddler((Data)data))
-                .addingCondition(isHuman).toRun(data -> functions.processDataForHuman((Data)data))
+        return FirstMatchingRuleDataFlow.Builder.<Data, String>create()
+                .addingCondition(isBaby).toRun(functions::processDataForBaby)
+                .addingCondition(isToddler).toRun(functions::processDataForToddler)
+                .addingCondition(isHuman).toRun(functions::processDataForHuman)
                 .build();
-
     }
 
     private Predicate<Data> isAgeInRange(int lower, int upper) {
--- src/main/java/eeethirdRefactor/executionFlow/FirstMatchingRuleDataFlow.java
+++ src/main/java/eeethirdRefactor/executionFlow/FirstMatchingRuleDataFlow.java
@@ -1,6 +1,9 @@
 package eeethirdRefactor.executionFlow;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FirstMatchingRuleDataFlow<T, R> {
 
@@ -19,4 +22,29 @@ public class FirstMatchingRuleDataFlow<T, R> {
 
         return conditionalExecutorForData.execute.apply(data);
     }
+
+    static public class Builder<T1, R1> {
+
+        private Predicate<T1> testifShouldProcess;
+        private List<ConditionalExecutor<T1, R1>> conditionalExecutors = new ArrayList<>();
+
+        public static <T1, R1> Builder<T1, R1> create() {
+            return new Builder<T1, R1>();
+        }
+
+        public Builder<T1, R1> addingCondition(Predicate<T1> testifShouldProcess) {
+            this.testifShouldProcess = testifShouldProcess;
+            return this;
+        }
+
+        public Builder<T1, R1> toRun(Function<T1, R1> process) {
+            conditionalExecutors.add(new ConditionalExecutor<>(testifShouldProcess, process));
+            return this;
+        }
+
+        public FirstMatchingRuleDataFlow<T1, R1> build() {
+            return new FirstMatchingRuleDataFlow<>(conditionalExecutors);
+        }
+    }
+
 }
